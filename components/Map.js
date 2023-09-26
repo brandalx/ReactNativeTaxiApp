646d51459c2f341c7f1463e8d322ab3050c915cf
brndalx <113251192+brandalx@users.noreply.github.com>
2023-09-27 01:55:03 +0300

    feat: market

--- components/Map.js
+++ components/Map.js
@@ -8,15 +8,28 @@ const Map = () => {
   const origin = useSelector(selectOrigin);
   return (
     <MapView
+      accessibilityLiveRegion="assertive"
       style={tw`flex-1`}
       mapType="mutedStandard"
       initialRegion={{
-        latitude: origin.location.lat,
-        longitude: origin.location.lng,
+        latitude: origin?.location?.lat || 37.78825,
+        longitude: origin?.location?.lng || -122.4324,
         latitudeDelta: 0.005,
         longitudeDelta: 0.005,
       }}
-    />
+    >
+      {origin?.location && (
+        <Marker
+          title="Origin"
+          description={origin.description}
+          identifier="origin"
+          coordinate={{
+            latitude: origin.location.lat,
+            longitude: origin.location.lng,
+          }}
+        />
+      )}
+    </MapView>
   );
 };
 
