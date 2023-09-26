3e1d31680ca6e7782ce711d21740e2cd1f1a5bc9
brndalx <113251192+brandalx@users.noreply.github.com>
2023-09-26 18:40:13 +0300

    feat: logo

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,14 +1,19 @@
-import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import { Image, SafeAreaView, StyleSheet, Text, View } from "react-native";
 import tw from "twrnc";
 
 import React from "react";
 
 const HomeScreen = () => {
   return (
-    <SafeAreaView>
-      <Text style={(tw`text-red-500 p-10,`, { color: "purple" })}>
-        This is home screen
-      </Text>
+    <SafeAreaView style={tw`bg-white h-full`}>
+      <View style={tw`p-5`}>
+        <Image
+          style={{ width: 100, height: 50, resizeMode: "contain" }}
+          source={{
+            uri: "https://www.freepnglogos.com/uploads/tesla-logo-png-13.png",
+          }}
+        />
+      </View>
     </SafeAreaView>
   );
 };
