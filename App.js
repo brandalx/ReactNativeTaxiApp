4ff4e5e5027e4cc5c0d535bbfda0d109ba9cad80
brndalx <113251192+brandalx@users.noreply.github.com>
2023-09-26 18:22:38 +0300

    feat: slices, actions, redux store

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { Provider } from "react-redux";
+import { store } from "./store";
 export default function App() {
   return (
-    <Provider  store={}>
+    <Provider store={store}>
       <View style={styles.container}>
         <Text>taxi app init!</Text>
         {/* <StatusBar style="auto" /> */}
