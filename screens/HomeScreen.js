2976eff9ff685454543c49ea6244a96301ac8da6
brndalx <113251192+brandalx@users.noreply.github.com>
2023-09-26 18:32:33 +0300

    feat: twc and styles combined test

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,14 @@
 import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import tw from "twrnc";
+
 import React from "react";
 
 const HomeScreen = () => {
   return (
     <SafeAreaView>
-      <Text style={styles.text}>This is home screen</Text>
+      <Text style={(tw`text-red-500 p-10,`, { color: "purple" })}>
+        This is home screen
+      </Text>
     </SafeAreaView>
   );
 };
