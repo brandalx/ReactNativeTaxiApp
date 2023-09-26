21b53021642dd29e7897d6145ff1a53a7ca0f772
brndalx <113251192+brandalx@users.noreply.github.com>
2023-09-26 18:40:51 +0300

    fix: logo pos

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -8,7 +8,7 @@ const HomeScreen = () => {
     <SafeAreaView style={tw`bg-white h-full`}>
       <View style={tw`p-5`}>
         <Image
-          style={{ width: 100, height: 50, resizeMode: "contain" }}
+          style={{ width: 50, height: 50, resizeMode: "contain" }}
           source={{
             uri: "https://www.freepnglogos.com/uploads/tesla-logo-png-13.png",
           }}
