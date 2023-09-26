7534091d7535cfd6000c2b953f980a5df010843d
brndalx <113251192+brandalx@users.noreply.github.com>
2023-09-27 02:21:26 +0300

    feat: styles for text input

--- components/NavigateCard.js
+++ components/NavigateCard.js
@@ -1,14 +1,42 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, SafeAreaView } from "react-native";
 import React from "react";
-
+import tw from "twrnc";
+import { GOOGLE_MAPS_APIKEY } from "@env";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
 const NavigateCard = () => {
   return (
-    <View>
-      <Text>NavigateCard</Text>
-    </View>
+    <SafeAreaView style={tw`bg-white flex-1`}>
+      <Text style={tw`text-center py-5 text-xl`}>Good Morning, Name</Text>
+      <View style={tw`border-t border-gray-200 flex-shrink`}>
+        <View>
+          <GooglePlacesAutocomplete
+            styles={inputBoxStyles}
+            placeholder="Where to?"
+            nearbyPlacesAPI="GooglePlacesSearch"
+            debounce={400}
+          />
+        </View>
+      </View>
+    </SafeAreaView>
   );
 };
 
 export default NavigateCard;
 
-const styles = StyleSheet.create({});
+const inputBoxStyles = StyleSheet.create({
+  container: {
+    backgroundColor: "white",
+    paddingTop: 15,
+
+    flex: 0,
+  },
+  textInput: {
+    backgroundColor: "#DDDDDF",
+    borderRadius: 100,
+    fontSize: 14,
+  },
+  textInputContainer: {
+    paddingHorizontal: 20,
+    paddingBottom: 0,
+  },
+});
