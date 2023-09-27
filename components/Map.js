d202884ac8febbf07f79e678ed8f120c271e6c23
brndalx <113251192+brandalx@users.noreply.github.com>
2023-09-27 03:20:43 +0300

    feat: fitToSuppliedMarkers

--- components/Map.js
+++ components/Map.js
@@ -1,5 +1,5 @@
 import { View, Text } from "react-native";
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import MapView, { Marker } from "react-native-maps";
 import tw from "twrnc";
 import { useSelector } from "react-redux";
@@ -10,8 +10,16 @@ import { GOOGLE_MAPS_APIKEY } from "@env";
 const Map = () => {
   const origin = useSelector(selectOrigin);
   const destination = useSelector(selectDestination);
+  const mapRef = useRef();
+  useEffect(() => {
+    if (!origin || !destination) return;
+    mapRef.current.fitToSuppliedMarkers(["origin", "destination"], {
+      edgePadding: { top: 50, right: 50, bottom: 50, left: 50 },
+    });
+  }, [origin, destination]);
   return (
     <MapView
+      ref={mapRef}
       accessibilityLiveRegion="assertive"
       style={tw`flex-1`}
       mapType="mutedStandard"
@@ -40,6 +48,18 @@ const Map = () => {
           coordinate={{
             latitude: origin.location.lat,
             longitude: origin.location.lng,
+          }}
+        />
+      )}
+
+      {destination?.location && (
+        <Marker
+          title="Destination"
+          description={origin.description}
+          identifier="destination"
+          coordinate={{
+            latitude: destination.location.lat,
+            longitude: destination.location.lng,
           }}
         />
       )}
