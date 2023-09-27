b4fa348dff3b1c1e87300b04686da4f1a730b253
brndalx <113251192+brandalx@users.noreply.github.com>
2023-09-27 03:55:31 +0300

    feat: favorites

--- components/NavFavorites.js
+++ components/NavFavorites.js
@@ -1,5 +1,8 @@
-import { StyleSheet, Text, View } from "react-native";
+import { FlatList, StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { TouchableOpacity } from "react-native";
+import { Icon } from "react-native-elements";
+import tw from "twrnc";
 const data = [
   {
     id: "123",
@@ -10,16 +13,35 @@ const data = [
 
   {
     id: "456",
-    icon: "breifcase",
+    icon: "briefcase",
     location: "Work",
     destination: "London Eye, UK",
   },
 ];
 const NavFavorites = () => {
   return (
-    <View>
-      <Text>NavFavorites</Text>
-    </View>
+    <FlatList
+      ItemSeparatorComponent={() => (
+        <View style={[tw`bg-gray-200`, { height: 0.5 }]} />
+      )}
+      data={data}
+      keyExtractor={(item) => item.id}
+      renderItem={({ item: { location, destination, icon } }) => (
+        <TouchableOpacity style={tw`flex-row items-center p-5`}>
+          <Icon
+            style={tw`mr-4 rounded-full bg-gray-300 p-3`}
+            name={icon}
+            color="white"
+            type="ionicon"
+            size={16}
+          />
+          <View>
+            <Text style={tw`font-semibold`}>{location}</Text>
+            <Text style={tw` text-gray-500`}>{destination}</Text>
+          </View>
+        </TouchableOpacity>
+      )}
+    ></FlatList>
   );
 };
 
