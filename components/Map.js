b470779bedf72c482f827b4a7505e8ecceb66732
brndalx <113251192+brandalx@users.noreply.github.com>
2023-09-27 16:10:06 +0300

    feat: google api distance

--- components/Map.js
+++ components/Map.js
@@ -17,6 +17,20 @@ const Map = () => {
       edgePadding: { top: 50, right: 50, bottom: 50, left: 50 },
     });
   }, [origin, destination]);
+
+  useEffect(() => {
+    if (!origin || !destination) return;
+    const getTravelTime = async () => {
+      const URL = `https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=${origin.description}&destinations=${destination.description}&key=${GOOGLE_MAPS_APIKEY}`;
+
+      fetch(URL)
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+        });
+    };
+    getTravelTime();
+  }, [origin, destination, GOOGLE_MAPS_APIKEY]);
   return (
     <MapView
       ref={mapRef}
