1cbf0306977fd22e667d3060642ffbcb3866804f
brndalx <113251192+brandalx@users.noreply.github.com>
2023-09-27 15:21:53 +0300

    feat: ride options navbar

--- components/RideOptionsCard.js
+++ components/RideOptionsCard.js
@@ -1,12 +1,31 @@
-import { View, Text, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  TouchableOpacity,
+} from "react-native";
 import React from "react";
 import { Icon } from "react-native-elements";
 import tw from "twrnc";
+import { useNavigation } from "@react-navigation/native";
 const RideOptionsCard = () => {
+  const navigation = useNavigation();
   return (
-    <View>
-      <Text>Pick a ride</Text>
-    </View>
+    <SafeAreaView style={tw`bg-white flex-grow`}>
+      <View>
+        <TouchableOpacity
+          onPress={() => {
+            navigation.navigate("NavigateCard");
+            console.log("Button pressed");
+          }}
+          style={tw`absolute top-3 left-5 p-3 rounded-full z-50`}
+        >
+          <Icon name="chevron-left" type="fontawesome" size={30} />
+        </TouchableOpacity>
+        <Text style={tw`text-center py-5 text-xl`}>Select a Ride</Text>
+      </View>
+    </SafeAreaView>
   );
 };
 
